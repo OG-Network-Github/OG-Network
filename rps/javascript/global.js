52f2e7fb6418c19d61366801eff415c169a33190
OG-Network-Github <ognetwork.fun@gmail.com>
2024-07-25 21:24:27 +0530

    Add files via upload

---
+++ rps/javascript/global.js
@@ -0,0 +1,20 @@
+
+
+
+// let cursor = document.querySelector('.cursor')
+ 
+// document.addEventListener('mousemove', (e) =>{
+//    posX = e.clientX
+//    posY = e.clientY
+//    cursor.animate({
+//       left: `${posX}px`,
+//       top: `${posY}px`
+//    }, {duration: 0, fill: 'forwards'})
+// })
+
+console.log(document.querySelector('.select').getAttribute('data-choice'));
+document.addEventListener('click', () =>{
+   
+})
+
+const bgMusic = document.getElementById('bg-music');
